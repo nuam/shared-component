99a2d8a662b22e39177787b4facbb93574414a5f
nuam <wadim.gaponow@mail.ru>
2019-01-19 17:58:24 +0300

    clipboard example

---
+++ src/components/Clipboard/index.js
@@ -0,0 +1,12 @@
+// check: react-copy-to-clipboard
+
+// const copyStringToClipboard = (str) => {
+//     const el = document.createElement('textarea');
+//     el.value = str;
+//     el.setAttribute('readonly', '');
+//     el.style = { position: 'absolute', left: '-9999px' };
+//     document.body.appendChild(el);
+//     el.select();
+//     document.execCommand('copy');
+//     document.body.removeChild(el);
+//   };
