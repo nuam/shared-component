11ae03c0f422d05be96b6c022ce9f7bcf4e318b7
nuam <wadim.gaponow@mail.ru>
2019-01-23 12:01:41 +0300

    added header (nav)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import { Route, Link } from 'react-router-dom';
 
 import ButtonsPage from './containers/Buttons';
 
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* <header className='App-header'> */}
-          <Link to='/buttons'>Buttons</Link>
-
-        {/* </header> */}
-        <Route path='/buttons' component={ButtonsPage} />
+        <header className='App-header'>
+          <Link to='/button' className='link'>Button</Link>
+          <Link to='/checkboxe' className='link'>Checkboxe</Link>
+          <Link to='/clipboard' className='link'>Clipboard</Link>
+          <Link to='/file' className='link'>File</Link>
+          <Link to='/input' className='link'>Input</Link>
+          <Link to='/modal' className='link'>Modal</Link>
+          <Link to='/select' className='link'>Select</Link>
+          <Link to='/textarea' className='link'>Textarea</Link>
+        </header>
+        <div className='App-main'>
+          <Route path='/button' component={ButtonsPage} />
+        </div>
       </div>
     );
   }
