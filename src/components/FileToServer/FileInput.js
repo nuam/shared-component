223e4ccb2d8031bde2356dde7a1ffcf254432b82
nuam <wadim.gaponow@mail.ru>
2019-02-27 15:55:02 +0300

    changed dropzone

--- src/components/FileToServer/FileInput.js
+++ src/components/FileToServer/FileInput.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const FileInput = props => (
-  <input type="file" onChange={(event) => props.onChange(event)} multiple />
+  <input
+    type="file"
+    className="input-file"
+    onChange={(event) => props.onChange(event)}
+    multiple
+  />
 );
 
 export default FileInput;
--- src/components/FileToServer/index.js
+++ src/components/FileToServer/index.js
@@ -1,4 +1,5 @@
 import FileInput from './FileInput';
 import ReactDropzone from './ReactDropzone';
+import './styles.scss';
 
 export { FileInput, ReactDropzone }
